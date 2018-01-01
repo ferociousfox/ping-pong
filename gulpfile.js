1a9c0ec83006af1f6b8ec92ac7781c78fc98be4e
mp <michael.pinaud@gmail.com>
2017-12-31 16:50:43 -0800

    install and add gulp-concat while creating a concatInterface fucntion and change the jsBrowserify function

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,32 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/pingpong-interface.js'] })
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+  .pipe(concat('allConcat.js'))
+  .pipe(gulp.dest('./tmp'));
+});
+
+//THIS IS WHAT concatInterface LOOKED LIKE BEFORE ADDING * AS A "GLOBAL PATTERN" TO CONCATENATE AND BROWSERIFY ALL FILES IN THE JS FOLDER THAT ENDS WITH STRING
+// gulp.task('concatInterface', function() {
+//   return gulp.src(['./js/pingpong-interface.js', './js/signup-interface.js'])
+//     .pipe(concat('allConcat.js'))
+//     .pipe(gulp.dest('./tmp'));
+// });
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
 });
+
+////THIS IS WHAT jsBrowserify LOOKED LIKE BEFORE WE ADDED THE NEW concatInterface function
+// gulp.task('jsBrowserify', function() {
+//   return browserify({ entries: ['./js/pingpong-interface.js'] })
+//     .bundle()
+//     .pipe(source('app.js'))
+//     .pipe(gulp.dest('./build/js'));
+// });
---
+++ tmp/allConcat.js
@@ -0,0 +1,27 @@
+// front end
+
+var Calculator = require('./../js/pingpong.js').calculatorModule;
+
+$(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    //user input
+    var goal = $('#goal').val();
+    //new user object
+    var simpleCalculator = new Calculator("hot pink");
+    //new user output
+    var output = simpleCalculator.pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
+
+$(function(){
+  $('#signup').submit(function(event){
+    event.preventDefault();
+    var email = $('#email').val();
+    $('#signup').hide();
+    $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
+  });
+});
