592b7c56d05b3db045dd873f675e71aca1ad3e02
mikey pinaud and shane ryan <shane@>
2018-01-02 16:01:36 -0800

    add jasmine test for pino and pong

--- spec/counter-spec.js
+++ spec/counter-spec.js
@@ -1,10 +1,13 @@
 var Calculator = require('./../js/pingpong.js').calculatorModule;
 
 describe('Calculator', function() {
-
-  it('should test whether number count starts at 1 to user input number', function(){
-    var cal = new Calculator();
-    expect(cal.pingPong(3)).toEqual("1, 2, 3");
+  it('should test whether number count starts at 1 to pong', function(){
+    var calcu = new Calculator();
+    expect(calcu.pingPong(3)).toEqual([1, 2, 'ping']);
   });
 
+  it('should test whether number count starts at 1 to pong', function(){
+    var calcu = new Calculator();
+    expect(calcu.pingPong(5)).toEqual([1, 2, 'ping', 4, 'pong']);
+  });
 });
